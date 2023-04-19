907d79cfe2a853fdb65fb153590ef394de57047a
Janaina Oliveira <janaina@dnaxis.com.br>
2023-04-19 15:59:09 -0300

    feat: config environments

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders,HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Person } from '../models/person';
+import { environment } from 'src/environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -12,29 +13,29 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class PersonService {
-  url = 'https://localhost:7267/api/person';
+  private readonly BASE_URL = `${environment.BASE_URL}/person`;
 
   constructor(private http: HttpClient) { }
 
   GetAll(): Observable<Person[]> {
-    return this.http.get<Person[]>(this.url);
+    return this.http.get<Person[]>(this.BASE_URL);
   }
 
   GetById(id: string): Observable<Person> {
-    const apiUrl = `${this.url}/${id}`;
+    const apiUrl = `${this.BASE_URL}/${id}`;
     return this.http.get<Person>(apiUrl);
   }
 
   Save(person: Person): Observable<any> {
-    return this.http.post<Person>(this.url, person, httpOptions)
+    return this.http.post<Person>(this.BASE_URL, person, httpOptions)
   }
 
   Update(person: Person): Observable<any> {
-    return this.http.put<Person>(this.url, person, httpOptions)
+    return this.http.put<Person>(this.BASE_URL, person, httpOptions)
   }
 
   Delete(id: string): Observable<any> {
-    const apiUrl = `${this.url}/${id}`;
+    const apiUrl = `${this.BASE_URL}/${id}`;
     return this.http.delete(apiUrl, httpOptions);
   }
 }
