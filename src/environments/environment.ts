7e91dd20781dec7f5abcb233b8e4941a19a456af
Janaina Oliveira <janaina@dnaxis.com.br>
2023-04-19 16:48:42 -0300

    fix: temp environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  BASE_URL: 'https://localhost:7267/api',
+  // BASE_URL: 'https://localhost:7267/api',
+  BASE_URL: 'https://contatos-api.azurewebsites.net/',
 };
