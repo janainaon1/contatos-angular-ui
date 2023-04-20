f4ae3f6805bb056321d66b6c65a07ba8d9d248dd
Janaina Oliveira <janaina@dnaxis.com.br>
2023-04-20 18:27:30 -0300

    chore: add icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
@@ -16,10 +19,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { PersonService } from './services/person.service';
 import { PersonComponent } from './person/person.component';
 import { PersonFormComponent } from './person/person-form/person-form.component';
-import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ContactEmailComponent } from './contact/contact-email/contact-email.component';
 import { ContactPhoneComponent } from './contact/contact-phone/contact-phone.component';
 import { EmailContactService } from './services/email-contact.service';
+import { icons } from './shared/icons';
 
 @NgModule({
   declarations: [
@@ -41,6 +44,8 @@ import { EmailContactService } from './services/email-contact.service';
     NgxMaskDirective,
     NgxMaskPipe,
     TabsModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    NgxBootstrapIconsModule.pick(icons),
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       progressBar: true,
